e35f724f5c24e3cc6b287af359a12e9203fd3abe
Akash Madavi <akashmadavi2001@gmail.com>
2025-06-03 08:14:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-let btns = document.querySelectorAll('button');
 let toastbox = document.querySelector('.toastbox');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        let toast = document.createElement('div');
-        toast.classList.add('toast');
-        
-        if (btn.innerHTML == "Success") {
-            toast.innerHTML = "<img src='./check.png'> SuccessFully";
-            toast.classList.add('success');
-        }
-        else if (btn.innerHTML == "Error") {
-            toast.innerHTML = "<img src='./cross.png'> Error";
-            toast.classList.add('error');
-        }
-        else if (btn.innerHTML == "Invalid") {
-            toast.innerHTML = "<img src='./exclamation.png'> Invalid";
-            toast.classList.add('invalid');
-        }
-        else if (btn.innerHTML == "Info") {
-            toast.innerHTML = "<img src='./information.png'> Info";
-            toast.classList.add('info');
-        }
-        else{
-            toast.innerHTML = "";
-            toast.classList.remove('toast');
-        }
-        
-        toastbox.appendChild(toast);
+function success() {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.classList.add('success');
+    toast.innerHTML = "<img src='./check.png'> SuccessFully";
+    toastbox.appendChild(toast);
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
+}
 
-        setTimeout(() => {
-            toast.remove();
-        }, 5000);
-    })
-})
+function error() {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.classList.add('error');
+    toast.innerHTML = "<img src='./cross.png'> Error";
+    toastbox.appendChild(toast);
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
+}
+
+function invalid() {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.classList.add('invalid');
+    toast.innerHTML = "<img src='./exclamation.png'> Invalid";
+    toastbox.appendChild(toast);
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
+}
+
+function info() {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.classList.add('info');
+    toast.innerHTML = "<img src='./information.png'> Info";
+    toastbox.appendChild(toast);
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
+}
